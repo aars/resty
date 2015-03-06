ff5a62c59cb8dc561dee3dff50e5b4844dc694d0
Adriean Khisbe <adriean.khisbe@live.fr>
2015-03-06 15:27:01 +0100

    tests: convert test file to shpec (stub matcher)

---
+++ shpec/matchers/restytest.sh
@@ -0,0 +1,9 @@
+# Shpec Matchers for resty
+restytest(){
+    # Lance commande, vérifie bonne requete
+
+    assert test true
+    # lance commande, vérifie bon output
+
+}
+# TOOD: use assert equal
