2c8ef4bab50bcf10ec2b1485885cc897ac9bbf44
Adriean Khisbe <adriean.khisbe@live.fr>
2015-03-06 16:13:31 +0100

    tests: reorganisation in progress

--- shpec/matchers/restytest.sh
+++ shpec/matchers/restytest.sh
@@ -1,9 +1,26 @@
 # Shpec Matchers for resty
 restytest(){
     # Lance commande, vérifie bonne requete
-
-    assert test true
+    local n=$1
+    local cmd=$2
     # lance commande, vérifie bon output
 
+    echo eval resty-call $cmd > "/tmp/test$n"
+
+    # §TODO: retrieve test
+
+    # TODO: Launch server
+    # netcat -l -w 2 -p 9090 -e './lilhttpd -I' localhost || break
+   # grep '^# TEST [0-9][0-9]*' $TEST |head -$j |tail -1 |sed 's/^#/# [REQ]/'
+
+    #netcat -l -w 2 -p 9090 -e ./lilhttpd localhost || break
+    #grep '^# TEST [0-9][0-9]*' $TEST |head -$j |tail -1 |sed 's/^#/# [OUT]/'
+  assert test true
 }
 # TOOD: use assert equal
+
+
+restytest_cmd(){
+    # command to be eval?
+    assert test true
+}
