8b6e5d8f8ab794e82b946a3100b6b73c05e06263
Aaron Heesakkers <aaronheesakkers@gmail.com>
2016-01-19 13:20:39 +0100

    Adds TestAPI http server for tests and step definitions to set them up. Split features into seperate files.

--- features/step_definitions/resty_steps.rb
+++ features/step_definitions/resty_steps.rb
@@ -1,10 +1,35 @@
-
-
-When(/^I call `resty([^`]*)`$/) do |resty_arg|
-  run_simple("bash -c '. ../../resty ; resty #{resty_arg}'", fail_on_error: false)
-end
-
 After do
   # Clean up config dir.
   FileUtils.rm_rf ENV['RESTY_TMP_CONF_HOME']
+end
+
+Given(/^resty is pointed at (.*)$/) do |url|
+  @resty_url = url
+end
+
+Given(/^I have a server on port (\d+)(?: with the following resources)?$/) do |port, resources|
+  TestAPI.start port
+
+  resources.hashes.each{ |r|
+    steps %Q{
+      Given the server responds to #{r[:method]} on #{r[:path]} with status #{r[:status]} and headers '#{r[:headers]}' and body
+      """"
+      #{r[:body]}
+      """"
+    }
+  }
+end
+
+Given(/^the server responds to ([A-Z]+) on (\/.+) with status ([0-9]{3})?(?: and headers '(.+)')?(?: and body)?$/) do
+  |method, path, status, headers, body|
+
+  options = {
+    :status => status,
+    :headers => headers
+  }
+  TestAPI.resource(method, path, body, options)
+end
+
+When(/^I call `([^`]*)`$/) do |cmd|
+  run_simple("bash -c '. ../../resty #{@resty_url}; #{cmd}'", fail_on_error: false)
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,3 +3,50 @@ require 'aruba/cucumber'
 ENV['RESTY_TMP_CONF_HOME'] = '/tmp/resty-cucumber'
 ENV['XDG_CONFIG_HOME']     = ENV['RESTY_TMP_CONF_HOME'] + '/xdg_config_home'
 ENV['XDG_DATA_HOME']       = ENV['RESTY_TMP_CONF_HOME'] + '/xdg_data_home'
+
+require 'webrick'
+class TestAPI
+  @resources = {}
+
+  def self.resource(method, path, body, options = {})
+    @resources["#{method}:#{path}"] = { body: body, options: options }
+  end
+
+  def self.clear
+    @resources = {}
+  end
+
+  def self.start(port)
+    server = WEBrick::HTTPServer.new(
+      Port: port,
+      Logger: WEBrick::Log.new('/dev/null'),
+      AccessLog: [nil, nil]
+    )
+
+    server.mount_proc '/' do |req, res|
+      resource = @resources["#{req.request_method}:#{req.path}"]
+
+      unless resource
+        res.status = 404
+      else
+        options = resource[:options]
+
+        if options[:status]
+          res.status = options[:status]
+        end
+
+        if options[:headers]
+          options[:headers].split(',').each{|h|
+            res[h.split(':')[0].strip] = h.split(':')[1].strip
+          }
+        end
+
+        res.body = resource[:body]
+      end
+    end
+
+    Thread.new do
+      server.start
+    end
+  end
+end
